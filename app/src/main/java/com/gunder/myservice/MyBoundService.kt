07c5f222f774bcb633ff9f4db5d1c2d50234e054
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-29 20:51:05 +0700

    membuat class MyBoundServicE

--- app/src/main/java/com/gunder/myservice/MyBoundService.kt
+++ app/src/main/java/com/gunder/myservice/MyBoundService.kt
@@ -2,11 +2,57 @@ package com.gunder.myservice
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
 
 class MyBoundService : Service() {
+    companion object {
+        private val TAG = MyBoundService::class.java.simpleName
+    }
+
+    private var mBinder = Binder()
+    private var startTime = System.currentTimeMillis()
+    private var mTimer: CountDownTimer = object : CountDownTimer(100000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            val elapsedTime = System.currentTimeMillis() - startTime
+            Log.d(TAG, "onClick: $elapsedTime")
+        }
+
+        override fun onFinish() {
+
+        }
+    }
+
+    internal inner class MyBinder : Binder() {
+        val getService: MyBoundService = this@MyBoundService
+    }
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Log.d(TAG, "onBind")
+        mTimer.start()
+        return mBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG,"onUnBind")
+        mTimer.cancel()
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+        Log.d(TAG, "onRebind")
     }
 }
