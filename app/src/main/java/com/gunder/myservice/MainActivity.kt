0a0f48cdeb293e49747c9c4f3e65d3ee9ea72586
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-29 16:05:20 +0700

    mnmbhkan inisialisasi button dan listener

--- app/src/main/java/com/gunder/myservice/MainActivity.kt
+++ app/src/main/java/com/gunder/myservice/MainActivity.kt
@@ -2,10 +2,23 @@ package com.gunder.myservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        inisialisasi button
+        val btnStartService = findViewById<Button>(R.id.btn_start_service)
+        btnStartService.setOnClickListener {  }
+
+        val btnStartJonIntentService = findViewById<Button>(R.id.btn_start_job_intent_service)
+        btnStartJonIntentService.setOnClickListener {  }
+
+        val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
+        btnStartBoundService.setOnClickListener {  }
+
+        val btnStopBoundService = findViewById<Button>(R.id.btn_stop_bound_service)
+        btnStopBoundService.setOnClickListener {  }
     }
 }
