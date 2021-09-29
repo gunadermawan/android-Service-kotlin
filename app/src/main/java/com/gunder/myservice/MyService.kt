92a1eb41847ec03bcf2c9c66973e967c34bd0db0
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-29 16:28:16 +0700

    mnbhakn coroutine

--- app/src/main/java/com/gunder/myservice/MyService.kt
+++ app/src/main/java/com/gunder/myservice/MyService.kt
@@ -4,6 +4,8 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
+import java.util.function.LongFunction
 
 class MyService : Service() {
 
@@ -11,12 +13,26 @@ class MyService : Service() {
         internal val TAG = MyService::class.java.simpleName
     }
 
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("not yet implemented")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "service dijalankan...")
+        serviceScope.launch {
+            delay(300)
+            stopSelf()
+            Log.d(TAG, "service dihentikan...")
+        }
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "on Destroy by system")
     }
 }
